0c2b7f55d0052d3782939e4ea0fde3115f168ce2
Michal <staskowiak@gmail.com>
2018-04-02 15:18:35 +0200

    Add files via upload

---
+++ core.py
@@ -0,0 +1,13 @@
+from S_W import SmithWater
+
+help(SmithWater)
+    storage = SmithWater()
+
+storage.file_names()
+storage.reading()
+storage.local_alignmet()
+storage.save_results("results.txt")
+
+
+
+
